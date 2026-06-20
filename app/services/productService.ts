df99a1637f6f1118281dfab25ea384a703833c1f
Whutai <taiphantmt@gmail.com>
2026-06-20 12:44:23 +0700

    update

--- app/services/productService.ts
+++ app/services/productService.ts
@@ -12,7 +12,7 @@ export const productService = {
     name: string;
     description: string;
     price: string;
-    image: string; // Sửa từ image_url thành image (khớp với page.tsx)
+    image?: string; // Sửa từ image_url thành image (khớp với page.tsx)
     target: string;
     pain_point: string;
     styling: string;
